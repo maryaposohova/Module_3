9fa997193844a7d2db3130dd6ad764094adba703
maryaposohova <maryaposohova1202@gmail.com>
2024-08-26 23:42:43 +0500

    Add files via upload

--- 3_модуль_урок_Произвольное_число_параметров_суммы_чисел_квадратов_кубов.py
+++ 3_модуль_урок_Произвольное_число_параметров_суммы_чисел_квадратов_кубов.py
@@ -33,22 +33,41 @@
 # нахождение суммы чисел, суммы квадратов, суммы кубов, тюе эта ф-я будет принимать какую-то степень этих чисел
 # n - степень, *args -какое то колво произвольных параметров и txt - именованный парамент текст.
 
-def my_sum(n, *args, txt="Сумма чисел"):
-    s = 0  # создадим переменную с 0, к которому будем приплюсовывать
-    for i in range(len(args)):  # здесь используем длину, так как args передаются в виде кортежа
-        s += args[i] ** n  # мы будем брать из наших args, возводить его а степень n и прибавлять к нашей переменной s
-    print(txt + ":", s)
-my_sum(1, 1,2,3,4,5)
-my_sum(2, 1,2,3,4,5, txt='Сумма квадратов') # Сумма квадратов, n - степень 2
-my_sum(3, 1,2,3,4,5, txt='Сумма кубов') # Сумма кубов, n - степень 3
+# def my_sum(n, *args, txt="Сумма чисел"):
+#     s = 0  # создадим переменную с 0, к которому будем приплюсовывать
+#     for i in range(len(args)):  # здесь используем длину, так как args передаются в виде кортежа
+#         s += args[i] ** n  # мы будем брать из наших args, возводить его а степень n и прибавлять к нашей переменной s
+#     print(txt + ":", s)
+# my_sum(1, 1,2,3,4,5)
+# my_sum(2, 1,2,3,4,5, txt='Сумма квадратов') # Сумма квадратов, n - степень 2
+# my_sum(3, 1,2,3,4,5, txt='Сумма кубов') # Сумма кубов, n - степень 3
 
 # Самостоятельная работа. Задача "Однокоренные":
 
-def single_root_words(root_word , *other_words):
-    same_words =[]
-    for i in range(len(other_words)):
-        same_words += other_words
-    print(same_words)
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_word.lower() in i.lower():  # с нижним регистром
+            same_words.append(i.lower())  # переводим в нижний и добавляем в список
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
+
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_word.lower() in i.lower():  # с нижним регистром
+            same_words.append(i)  #  добавляем в список
+        if i.lower() in root_word.lower():  # с нижним регистром
+            same_words.append(i)  #  добавляем в список
+    return same_words
+
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
