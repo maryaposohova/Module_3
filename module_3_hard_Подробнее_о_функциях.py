0697b2b1373330d8993fd131aa32c6bd8349fd20
maryaposohova <maryaposohova1202@gmail.com>
2024-08-30 23:56:48 +0500

    Add files via upload

--- module_3_hard_Подробнее_о_функциях.py
+++ module_3_hard_Подробнее_о_функциях.py
@@ -119,52 +119,174 @@ a = []
 # print()
 
 
+''' Нашла значение для слова Hello'''
+
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                     # ((), [{(2, 'Urban', ('Urban2', 35))}])
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                   # {(2, 'Urban', ('Urban2', 35))}
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      # Hello
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                   # o
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                    # o     не сработало
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                       # (6, {'cube': 7, 'drum': 8})
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                    # {'cube': 7, 'drum': 8}
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                    #  drum
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                       # {'a': 4, 'b': 5}
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                    # b
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                     # b  не сработало
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      # [1, 2, 3]
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                    # 3
+# last_2= list(str(last_1)).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                     # 3
+#
+# print()
+
+
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                     # ((), [{(2, 'Urban', ('Urban2', 35))}])
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                   # {(2, 'Urban', ('Urban2', 35))}
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      # Hello
+# if isinstance(last, str) == True:
+#     print(list(last))
+# print(len(list(last)))                                                       # нашли значение
+#
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                   # o
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                    # o     не сработало
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                       # (6, {'cube': 7, 'drum': 8})
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                    # {'cube': 7, 'drum': 8}
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                    #  drum
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                       # {'a': 4, 'b': 5}
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                    # b
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                     # b  не сработало
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
+#
+# print()
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      # [1, 2, 3]
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                    # 3
+# last_2= list(str(last_1)).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                     # 3
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
+#
+# print()
+# print()
+# print()
+
+
+''' Примеряем со словом Хелло ко всем'''
 
 last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                     # ((), [{(2, 'Urban', ('Urban2', 35))}])
+print("Оставили", last)                                                      # Hello
+if isinstance(last, str) == True:
+    print(list(last))
+print(len(list(last)))                                                       # нашли значение
 last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
+print("Оставили", last_1)                                                   # o
 last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
-
-print()
+print("Оставили", last_2)                                                    # o     не сработало
+last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
 print()
 
 last = data_structure.pop()  # ытащили последний, и его надо разобрать
 print("Оставили", last)                                                      # Hello
+if isinstance(last, str) == True:
+    print(list(last))
+print(len(list(last)))                                                       # нашли значение
 last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
 print("Оставили", last_1)                                                   # o
 last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)
-
-print()
+print("Оставили", last_2)                                                    # o     не сработало
+last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
 print()
 
 last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                       # (6, {'cube': 7, 'drum': 8})
+print("Оставили", last)                                                      # Hello
+if isinstance(last, str) == True:
+    print(list(last))
+print(len(list(last)))                                                       # нашли значение
 last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                    # {'cube': 7, 'drum': 8}
+print("Оставили", last_1)                                                   # o
 last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)
-
-print()
+print("Оставили", last_2)                                                    # o     не сработало
+last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
 print()
 
 last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                       # {'a': 4, 'b': 5}
+print("Оставили", last)                                                      # Hello
+if isinstance(last, str) == True:
+    print(list(last))
+print(len(list(last)))                                                       # нашли значение
 last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                    # b
+print("Оставили", last_1)                                                   # o
 last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)
-
-print()
-print()
-
-last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                      # [1, 2, 3]
-last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                    # 3
-last_2= list(str(last_1)).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)
-
+print("Оставили", last_2)                                                    # o     не сработало
+last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
 print()
