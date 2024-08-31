deeb69ada9d1e00a70dc6a855b40b11f39674cb7
maryaposohova <maryaposohova1202@gmail.com>
2024-08-31 15:58:26 +0500

    Add files via upload

--- module_3_hard_Подробнее_о_функциях.py
+++ module_3_hard_Подробнее_о_функциях.py
@@ -9,7 +9,7 @@ data_structure = [
     "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-a = []
+sum = []
 # last = data_structure.pop()  # ытащили последний, и его надо разобрать
 # print("Оставили", last)
 # print("Убрали элемент1", data_structure)
@@ -64,7 +64,7 @@ a = []
 # print()
 # last = data_structure.pop()  # ытащили последний, и его надо разобрать
 # print("Оставили", last)
-
+#
 
 
 # #повторяющееся вытаскивание из общего списка
@@ -237,56 +237,285 @@ a = []
 # print()
 
 
-''' Примеряем со словом Хелло ко всем'''
+# ''' ==== Примеряем ко всем======'''
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      #  ((), [{(2, 'Urban', ('Urban2', 35))}])
+# if isinstance(last, int) == True:
+#     last = str(last)
+# print(len(list(last)))                                                       #  2  списка посчиталось
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрат
+# if isinstance(last_1, int) == True:
+#     last_1 = str(last_1)
+# print("Оставили", last_1)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_2, int) == True:
+#     last_2 = str(last_2)
+# print("Оставили", last_2)                                                    # {(2, 'Urban', ('Urban2', 35))}
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_3, int) == True:
+#     last_3 = str(last_3)
+# print("Оставили", last_3)                                                   # (2, 'Urban', ('Urban2', 35))
+# last_4= list(last_3).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_4, int) == True:
+#     last_4 = str(last_4)
+# print("Оставили", last_4)                                                    # ('Urban2', 35)
+# last_5= list(last_4).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_5, int) == True:
+#     last_5 = str(last_5)
+#     print("Оставили", last_5)                                                   # 35
+# last_6= list(last_5).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_6, int) == True:
+#     last_6 = str(last_6)
+# print("Оставили", str(last_6))
+#
+# print()
+#
+#
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      #  ((), [{(2, 'Urban', ('Urban2', 35))}])
+# if isinstance(last, int) == True:
+#     last = str(last)
+# print(len(list(last)))                                                       #  2  списка посчиталось
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрат
+# if isinstance(last_1, int) == True:
+#     last_1 = str(last_1)
+# print("Оставили", last_1)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_2, int) == True:
+#     last_2 = str(last_2)
+# print("Оставили", last_2)                                                    # {(2, 'Urban', ('Urban2', 35))}
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_3, int) == True:
+#     last_3 = str(last_3)
+# print("Оставили", last_3)                                                   # (2, 'Urban', ('Urban2', 35))
+# last_4= list(last_3).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_4, int) == True:
+#     last_4 = str(last_4)
+# print("Оставили", last_4)                                                    # ('Urban2', 35)
+# last_5= list(last_4).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_5, int) == True:
+#     last_5 = str(last_5)
+#     print("Оставили", last_5)                                                   # 35
+# last_6= list(last_5).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_6, int) == True:
+#     last_6 = str(last_6)
+# print("Оставили", str(last_6))
+#
+# print()
 
-last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                      # Hello
-if isinstance(last, str) == True:
-    print(list(last))
-print(len(list(last)))                                                       # нашли значение
-last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                   # o
-last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)                                                    # o     не сработало
-last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
-print()
 
-last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                      # Hello
-if isinstance(last, str) == True:
-    print(list(last))
-print(len(list(last)))                                                       # нашли значение
-last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                   # o
-last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)                                                    # o     не сработало
-last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
-print()
 
-last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                      # Hello
-if isinstance(last, str) == True:
-    print(list(last))
-print(len(list(last)))                                                       # нашли значение
-last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                   # o
-last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)                                                    # o     не сработало
-last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
-print()
 
-last = data_structure.pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last)                                                      # Hello
-if isinstance(last, str) == True:
-    print(list(last))
-print(len(list(last)))                                                       # нашли значение
-last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_1)                                                   # o
-last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_2)                                                    # o     не сработало
-last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
-print("Оставили", last_3)                                                   # {(2, 'Urban', ('Urban2', 35))}
-print()
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      #
+# if isinstance(last, str) == True:
+#     print(list(last))
+# print(len(list(last)))                                                       #
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)                                                   #
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                    #
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)                                                   #
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      #
+# if isinstance(last, str) == True:
+#     print(list(last))
+# print(len(list(last)))                                                       #
+# print("Оставили", last_1)                                                   #
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)                                                    #
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)
+# print()
+#
+# last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)
+# if isinstance(last, str) == True:
+#     print(list(last))
+# print(len(list(last)))
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_1)
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_2)
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last_3)
+# print()
+
+#  последнее
+
+#
+# Last = data_structure.pop()  # ытащили последний, и его надо разобрать
+# print("Оставили", last)                                                      #  ((), [{(2, 'Urban', ('Urban2', 35))}])
+# if isinstance(last, int) == True:
+#     last = str(last)
+# print(len(list(last)))                                                       #  2  списка посчиталось
+# last_1 = list(last).pop()  # ытащили последний, и его надо разобрат
+# if isinstance(last_1, int) == True:
+#     last_1 = str(last_1)
+# print("Оставили", last_1)                                                   # [{(2, 'Urban', ('Urban2', 35))}]
+# last_2= list(last_1).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_2, int) == True:
+#     last_2 = str(last_2)
+# print("Оставили", last_2)                                                    # {(2, 'Urban', ('Urban2', 35))}
+# last_3= list(last_2).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_3, int) == True:
+#     last_3 = str(last_3)
+# print("Оставили", last_3)                                                   # (2, 'Urban', ('Urban2', 35))
+# last_4= list(last_3).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_4, int) == True:
+#     last_4 = str(last_4)
+# print("Оставили", last_4)                                                    # ('Urban2', 35)
+# last_5= list(last_4).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_5, int) == True:
+#     last_5 = str(last_5)
+#     print("Оставили", last_5)                                                   # 35
+# last_6= list(last_5).pop()  # ытащили последний, и его надо разобрать
+# if isinstance(last_6, int) == True:
+#     last_6 = str(last_6)
+# print("Оставили", str(last_6))
+#
+# print()
+
+
+
+# last_external()
+# last_external()
+# last_external()
+# last_external()
+# last_external()
+
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, tuple):
+#         last = list(last)
+#         last_external()
+#     if isinstance(last, dict):
+#         last_external()
+#         last_external()
+#     if isinstance(last, int):
+#          sum_ = sum_+last
+#     if isinstance(last, str):
+#          sum_ = sum_+ int(len(last))
+
+
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#          sum_ = sum_+last
+#     elif isinstance(last, str):
+#          sum_ = sum_+ str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#          sum_ = sum_+last
+#     elif isinstance(last, str):
+#          sum_ = sum_+ str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#          sum_ = sum_+last
+#     elif isinstance(last, str):
+#          sum_ = sum_+ str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#         sum_ = sum_ + last
+#     elif isinstance(last, str):
+#         sum_ = sum_ + str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#         sum_ = sum_ + last
+#     elif isinstance(last, str):
+#         sum_ = sum_ + str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#         sum_ = sum_ + last
+#     elif isinstance(last, str):
+#         sum_ = sum_ + str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#         sum_ = sum_ + last
+#     elif isinstance(last, str):
+#         sum_ = sum_ + str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#         sum_ = sum_ + last
+#     elif isinstance(last, str):
+#         sum_ = sum_ + str(len(last))
+#
+# for last in data_structure:
+#     if isinstance(last, list):
+#         last_external()
+#     if isinstance(last, int):
+#         sum_ = sum_ + last
+#     elif isinstance(last, str):
+#         sum_ = sum_ + str(len(last))
+
+
+
+
+
+
+
+
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+sum_ = []
+def last_external(*list):
+    last = data_structure.pop()
+    print("Оставили", last)
+
+
+
+for last_ in data_structure:
+    for last in data_structure:
+        if isinstance(last, list):
+            last_external()
+        if isinstance(last, tuple):
+            last = list(last)
+            last_external()
+        if isinstance(last, dict):
+            last_external()
+        if isinstance(last, int):
+            sum_ = sum_ + last
+        if isinstance(last, str):
+            sum_ = sum_ + int(len(last))
+
+
+print(last)
+print(sum_)
+
