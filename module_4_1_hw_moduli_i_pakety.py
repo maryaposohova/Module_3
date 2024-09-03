c5c5162f1cd6b325eef417a7b412fa71ce169741
maryaposohova <maryaposohova1202@gmail.com>
2024-09-03 12:31:52 +0500

    Add files via upload

---
+++ module_4_1_hw_moduli_i_pakety.py
@@ -0,0 +1,11 @@
+import fake_math_xdel0_err as er
+import true_math_xdel0_inf_ as i
+
+result1 = er.divide(69, 3)
+result2 = er.divide(3, 0)
+result3 = i.divide(49, 7)
+result4 = i.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
